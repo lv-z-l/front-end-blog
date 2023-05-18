66579e9138d18275cc5ef129f19b5164887ef564
lvzl <627417163@qq.com>
2023-05-18 20:13:18 +0800

    refactor

--- .vitepress/cache/deps/@theme_index.js
+++ .vitepress/cache/deps/@theme_index.js
@@ -1,27 +1,27 @@
 import "./chunk-OL3AADLO.js";
 
-// node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+// node_modules/vitepress/dist/client/theme-default/index.js
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
-// node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/zllv/my-space/front-end-blog/node_modules/.pnpm/registry.npmmirror.com+vitepress@1.0.0-alpha.75_@algolia+client-search@4.17.0/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+// node_modules/vitepress/dist/client/theme-default/without-fonts.js
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "/Users/zllv/my-space/front-end-blog/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   enhanceApp: ({ app }) => {
--- articles/other/packagemanager.md
+++ articles/other/packagemanager.md
@@ -2,4 +2,4 @@
 title: 聊聊 npm & yarn & pnpm 包管理机制
 author: lvzl
 ---
-<img src="@pic/npm&yarn&pnpm.png" />
+<img src="https://mp-d22f2f25-96ec-4381-920f-a0d8df227b60.cdn.bspapp.com/cloudstorage/0112a686-55e0-4e6d-a164-b79c45f17453.jpg" />
